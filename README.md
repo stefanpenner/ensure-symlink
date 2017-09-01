dd09330c0b3ef738e4f414f6166c113facdbbc97
Stefan Penner <stefan.penner@gmail.com>
2017-09-01 14:12:28 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# ensureSymlink
+
+used to implement a resilient "best-effort last write wins" symlink
+
+## Usage
+
+```sh
+yarn add ensure-symlink
+# or
+
+npm install ensure-symlink
+```
+
+```js
+const ensureSymlink = require('ensure-symlink');
+
+ensureSymlink(from, to);
+```
+
+
---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const fs = require('fs');
+
+function readlinkSyncOrNull(path) {
+  try {
+    return fs.readlinkSync(path);
+  } catch (e) {
+    if (typeof e === 'object' && e !== null && e.code === 'ENOENT') {
+      // no LINK exists
+      return null;
+    }
+    throw e;
+  }
+}
+
+function symlinkSyncUnlessPresent(from, to) {
+  try {
+    fs.symlinkSync(from, to);
+  } catch (e) {
+    if (typeof e === 'object' && e !== null && e.code === 'EEXIST') {
+      return;
+    } else {
+      throw e;
+    }
+  }
+}
+
+function unlinkIfPresent(path) {
+  try {
+    fs.unlinkSync(path);
+  } catch (e) {
+    if (typeof e === 'object' && e !== null && e.code === 'ENOENT') {
+      return;
+    } else {
+      throw e;
+    }
+  }
+}
+
+module.exports = ensureSymlinkSync;
+function ensureSymlinkSync(from, to) {
+  let existingFrom = readlinkSyncOrNull(to);
+  if (existingFrom === from) {
+    return;
+  }
+  if (existingFrom) {
+    unlinkIfPresent(to);
+  }
+  symlinkSyncUnlessPresent(from, to);
+}
